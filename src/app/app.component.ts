e79224bd8a79ce07bdd1f5f416e18cd323dd7e1c
daveo30 <devanshojha30@gmail.com>
2019-08-03 19:08:06 +0530

    Basic UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'search';
+  httpClient: HttpClient;
+
+  startSearch (searchInput: string) {
+     this.httpClient.post("url", searchInput);
+  
+  }
+
+    
+  
 }
